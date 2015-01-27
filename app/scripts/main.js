30d136c97f0224c6e6a7ee7073d7ee9aaa3e7859
Thadeu Brito <itvbrito@gmail.com>
2015-01-27 19:11:41 -0300

    Travel's informations section

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,21 +1,21 @@
 console.log('\'Allo \'Allo!');
 
-$('.months').countdown('2015/10/10', function(event) {
+$('.months').countdown('2015/07/04', function(event) {
   $(this).html(event.strftime('%m'));
 });
 
-$('.days').countdown('2015/10/10', function(event) {
+$('.days').countdown('2015/07/04', function(event) {
   $(this).html(event.strftime('%d'));
 });
 
-$('.hours').countdown('2015/10/10', function(event) {
+$('.hours').countdown('2015/07/04', function(event) {
   $(this).html(event.strftime('%H'));
 });
 
-$('.minutes').countdown('2015/10/10', function(event) {
+$('.minutes').countdown('2015/07/04', function(event) {
   $(this).html(event.strftime('%M'));
 });
 
-$('.seconds').countdown('2015/10/10', function(event) {
+$('.seconds').countdown('2015/07/04', function(event) {
   $(this).html(event.strftime('%S'));
 });
