d78f4f57103cd7257e1c53f10a853201095de1ad
Thadeu Brito <itvbrito@gmail.com>
2015-01-26 23:37:57 -0300

    Count down section

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,21 @@
 console.log('\'Allo \'Allo!');
+
+$('.months').countdown('2015/10/10', function(event) {
+  $(this).html(event.strftime('%m'));
+});
+
+$('.days').countdown('2015/10/10', function(event) {
+  $(this).html(event.strftime('%d'));
+});
+
+$('.hours').countdown('2015/10/10', function(event) {
+  $(this).html(event.strftime('%H'));
+});
+
+$('.minutes').countdown('2015/10/10', function(event) {
+  $(this).html(event.strftime('%M'));
+});
+
+$('.seconds').countdown('2015/10/10', function(event) {
+  $(this).html(event.strftime('%S'));
+});
