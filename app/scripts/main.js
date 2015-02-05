e369be089e952af74a71a17189c27ca61520e9d4
Thadeu Brito <itvbrito@gmail.com>
2015-02-05 00:48:13 -0300

    Added google maps

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -19,3 +19,22 @@ $('.minutes').countdown('2015/07/04', function(event) {
 $('.seconds').countdown('2015/07/04', function(event) {
   $(this).html(event.strftime('%S'));
 });
+
+
+function initialize() {
+  var myLatlng = new google.maps.LatLng(-2.501713, -44.237152);
+  var mapOptions = {
+    zoom: 15,
+    scrollwheel: false,
+    center: myLatlng
+  }
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+  var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: 'Hello World!'
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
