4b9883ba4690c3a12bdaec17a06f02eadfa51f3c
Thadeu Brito <itvbrito@gmail.com>
2015-04-05 14:29:11 -0300

    Update heroku setup

--- web.js
+++ web.js
@@ -1,7 +1,13 @@
 var gzippo = require('gzippo');
 var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
 var app = express();
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
-app.use(express.logger('dev'));
+app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
