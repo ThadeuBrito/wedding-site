11e44b8ac890a81ccd36386fe9ced20d80857dcd
Thadeu Brito <itvbrito@gmail.com>
2015-05-05 00:15:22 -0300

    Removed facebook plugin

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,21 +2,21 @@
 
 console.log('\'Allo \'Allo!');
 
-window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '1437399506573620',
-    xfbml      : true,
-    version    : 'v2.3'
-  });
-};
-
-(function(d, s, id){
-   var js, fjs = d.getElementsByTagName(s)[0];
-   if (d.getElementById(id)) {return;}
-   js = d.createElement(s); js.id = id;
-   js.src = "//connect.facebook.net/en_US/sdk.js";
-   fjs.parentNode.insertBefore(js, fjs);
- }(document, 'script', 'facebook-jssdk'));
+// window.fbAsyncInit = function() {
+//   FB.init({
+//     appId      : '1437399506573620',
+//     xfbml      : true,
+//     version    : 'v2.3'
+//   });
+// };
+//
+// (function(d, s, id){
+//    var js, fjs = d.getElementsByTagName(s)[0];
+//    if (d.getElementById(id)) {return;}
+//    js = d.createElement(s); js.id = id;
+//    js.src = "//connect.facebook.net/en_US/sdk.js";
+//    fjs.parentNode.insertBefore(js, fjs);
+//  }(document, 'script', 'facebook-jssdk'));
 
 
 // $('.months').countdown('2015/7/4', function(event) {
